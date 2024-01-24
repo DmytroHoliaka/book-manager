3cccceec01867b890b537b140d8d7e39b16b7e92
HoliakaDmytro <holidim39@gmail.com>
2024-01-24 15:07:11 +0200

    Context: Create ApplicationContext for configuration

---
+++ BookManager/Context/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using BookManager.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BookManager.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Author> Authors => Set<Author>();
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Genre> Genres => Set<Genre>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("Configurations.json")
+                                        .Build();
+
+            string? connectionLine = config.GetConnectionString("Default");
+            optionsBuilder.UseSqlServer(connectionLine ?? 
+                throw new ArgumentNullException(nameof(connectionLine), "Cannot find connection line"));
+        }
+    }
+}
