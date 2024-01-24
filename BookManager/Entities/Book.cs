5bcd9c5818f623c0b6cf93f0c9df5d8a383e9909
HoliakaDmytro <holidim39@gmail.com>
2024-01-24 15:06:36 +0200

    Entities: Add Author, Book, Genre and Publisher classes

---
+++ BookManager/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace BookManager.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }      // ToDo: IsRequired()
+        public int Pages { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public int GenreId { get; set; }        
+        public Genre? Genre { get; set; }
+
+        public int AuthorId { get; set; }       
+        public Author? Author { get; set; }
+
+        public int PublisherId { get; set; }   
+        public Publisher? Publisher { get; set; }
+    }
+}
---
+++ BookManager/Entities/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManager.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }   // ToDo: IsRequired()
+
+        public ICollection<Book>? Books { get; set; }
+    }
+}
