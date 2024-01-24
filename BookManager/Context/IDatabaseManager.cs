92b420f5c755aac8deaa49049e157b11817af5eb
HoliakaDmytro <holidim39@gmail.com>
2024-01-24 17:54:39 +0200

    IDatabaseManager: Add interface for working with database

---
+++ BookManager/Context/IDatabaseManager.cs
@@ -0,0 +1,12 @@
+﻿using BookManager.Entities;
+
+namespace BookManager.Context
+{
+    public interface IDatabaseManager
+    {
+        int InsertAuthor(string authorName);
+        int InsertGenre(string genreName);
+        int InsertPublisher(string publisherName);
+        int InsertBook(string title, int pages, DateTime releaseDate, int genre, int author, int publisher);
+    }
+}
