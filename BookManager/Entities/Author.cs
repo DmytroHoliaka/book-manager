7c91e5c5e337dd93651bdc803859bb21075171c8
HoliakaDmytro <holidim39@gmail.com>
2024-01-26 16:54:33 +0200

    Author: Add entity copy constructor

--- BookManager/Entities/Author.cs
+++ BookManager/Entities/Author.cs
@@ -3,8 +3,26 @@
     public class Author
     {
         public int Id { get; set; }
-        public string? Name { get; set; }   // ToDo: IsRequired()
+        public string? Name { get; set; }   // Required
 
         public ICollection<Book>? Books { get; set; }
+
+        public Author()
+        {
+
+        }
+
+        public Author(Author? origin)
+        {
+            if (origin is null)
+            {
+                throw new ArgumentNullException(nameof(origin), "Origin instance of author cannot be null");
+            }
+
+            Id = origin.Id;
+            Name = origin.Name;
+
+            Books = origin.Books is null ? null : new List<Book>(origin.Books);
+        }
     }
 }
