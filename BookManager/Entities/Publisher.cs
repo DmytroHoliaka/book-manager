c3e5a31dacbd72f2222944d00670da66f0100a82
HoliakaDmytro <holidim39@gmail.com>
2024-01-26 16:55:03 +0200

    Publisher: Add entity copy constructor

--- BookManager/Entities/Publisher.cs
+++ BookManager/Entities/Publisher.cs
@@ -3,8 +3,26 @@
     public class Publisher
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string? Name { get; set; }   // Required
 
         public ICollection<Book>? Books { get; set; }
+
+        public Publisher()
+        {
+
+        }
+
+        public Publisher(Publisher? origin)
+        {
+            if (origin is null)
+            {
+                throw new ArgumentNullException(nameof(origin), "Origin instance of publisher cannot be null");
+            }
+
+            Id = origin.Id;
+            Name = origin.Name;
+
+            Books = origin.Books is null ? null : new List<Book>(origin.Books);
+        }
     }
 }
