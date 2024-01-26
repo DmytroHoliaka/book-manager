d730619c342101af6122c2db3343e3b4af30788a
HoliakaDmytro <holidim39@gmail.com>
2024-01-26 16:56:47 +0200

    General: Remove classes for working with database

---
+++ BookManager/Database/DatabaseManager.cs
@@ -0,0 +1,110 @@
+﻿using BookManager.Entities;
+
+namespace BookManager.Database
+{
+    public class DatabaseManager : IDatabaseManager
+    {
+        private readonly ApplicationContext _db;
+
+        public DatabaseManager(ApplicationContext db)
+        {
+            if (db is null)
+            {
+                throw new ArgumentNullException(nameof(db), "Application context cannot be null");
+            }
+
+            _db = db;
+        }
+
+
+        public int InsertAuthor(string authorName)
+        {
+            Author? duplicate = _db.Authors.FirstOrDefault(a => a.Name == authorName);
+
+            if (duplicate is not null)
+            {
+                return duplicate.Id;
+            }
+
+            Author author = new()
+            {
+                Name = authorName,
+            };
+
+            _db.Authors.Add(author);
+            _db.SaveChanges();
+
+            return author.Id;
+        }
+
+        public int InsertGenre(string genreName)
+        {
+            Genre? duplicate = _db.Genres.FirstOrDefault(d => d.Name == genreName);
+
+            if (duplicate is not null)
+            {
+                return duplicate.Id;
+            }
+
+            Genre genre = new()
+            {
+                Name = genreName,
+            };
+
+            _db.Genres.Add(genre);
+            _db.SaveChanges();
+
+            return genre.Id;
+        }
+
+        public int InsertPublisher(string publisherName)
+        {
+            Publisher? duplicate = _db.Publishers.FirstOrDefault(p => p.Name == publisherName);
+
+            if (duplicate is not null)
+            {
+                return duplicate.Id;
+            }
+
+            Publisher publisher = new()
+            {
+                Name = publisherName,
+            };
+
+            _db.Publishers.Add(publisher);
+            _db.SaveChanges();
+
+            return publisher.Id;
+        }
+
+        public int InsertBook(string title, int pages, DateTime? releaseDate, int genreId, int? authorId, int? publisherId)
+        {
+            Book? duplicate = _db.Books.FirstOrDefault(b => b.Title == title &&
+                                                            b.Pages == pages &&
+                                                            b.ReleaseDate == releaseDate &&
+                                                            b.GenreId == genreId &&
+                                                            b.AuthorId == authorId &&
+                                                            b.PublisherId == publisherId);
+
+            if (duplicate is not null)
+            {
+                return duplicate.Id;
+            }
+
+            Book book = new()
+            {
+                Title = title,
+                Pages = pages,
+                ReleaseDate = releaseDate,
+                GenreId = genreId,
+                AuthorId = authorId,
+                PublisherId = publisherId,
+            };
+
+            _db.Books.Add(book);
+            _db.SaveChanges();
+
+            return book.Id;
+        }
+    }
+}
---
+++ BookManager/Database/IDatabaseManager.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManager.Database
+{
+    public interface IDatabaseManager
+    {
+        int InsertAuthor(string authorName);
+        int InsertGenre(string genreName);
+        int InsertPublisher(string publisherName);
+        int InsertBook(string title, int pages, DateTime? releaseDate, int genre, int? author, int? publisher);
+    }
+}
