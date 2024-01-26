8cc9b97ff7b7c183c13e0e19376f6abff2d5456d
HoliakaDmytro <holidim39@gmail.com>
2024-01-26 16:52:37 +0200

    General: Rename context folder

---
+++ BookManager/Configuration/ConfiguratorJson.cs
@@ -0,0 +1,29 @@
+﻿using BookManager.Filtering;
+using Microsoft.Extensions.Configuration;
+
+namespace BookManager.Configuration
+{
+    public class ConfiguratorJson : IConfigurator
+    {
+        public IConfiguration Config { get; }
+
+        public ConfiguratorJson(string jsonName)
+        {
+            Config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory() + @"\Configuration\")
+                .AddJsonFile(jsonName)
+                .Build();
+        }
+
+        public IConfiguration GetConfig()
+        {
+            return Config;
+        }
+
+        public Filter GetFilter(string sectionName)
+        {
+            return Config.GetSection(sectionName).Get<Filter>() ??
+                throw new InvalidDataException("Cannot create filter from json file");
+        }
+    }
+}
---
+++ BookManager/Configuration/IConfigurator.cs
@@ -0,0 +1,12 @@
+﻿using BookManager.Filtering;
+using Microsoft.Extensions.Configuration;
+
+namespace BookManager.Configuration
+{
+    public interface IConfigurator
+    {
+        IConfiguration Config { get; }
+
+        Filter GetFilter(string sectionName);
+    }
+}
