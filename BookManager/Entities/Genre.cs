23a29c3fffd2cf3df0429b39779773717f8d1f27
HoliakaDmytro <holidim39@gmail.com>
2024-01-26 16:54:55 +0200

    Genre: Add entity copy constructor

--- BookManager/Entities/Genre.cs
+++ BookManager/Entities/Genre.cs
@@ -3,8 +3,26 @@
     public class Genre
     {
         public int Id { get; set; }
-        public string? Name { get; set; }   // ToDo: IsRequired()
+        public string? Name { get; set; }   // Required   
 
         public ICollection<Book>? Books { get; set; }
+
+        public Genre()
+        {
+
+        }
+
+        public Genre(Genre? origin)
+        {
+            if (origin is null)
+            {
+                throw new ArgumentNullException(nameof(origin), "Origin instance of genre cannot be null");
+            }
+
+            Id = origin.Id;
+            Name = origin.Name;
+
+            Books = origin.Books is null ? null : new List<Book>(origin.Books); 
+        }
     }
 }
