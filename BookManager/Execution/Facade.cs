a28a4ba26a2c24ce204ac1db914920ba230859bd
HoliakaDmytro <holidim39@gmail.com>
2024-01-28 13:19:32 +0200

    Facade: Add a simplified user interface

---
+++ BookManager/Execution/Facade.cs
@@ -0,0 +1,39 @@
+﻿using BookManager.Configuration;
+using BookManager.Database;
+using BookManager.FileProcessing;
+using BookManager.Filtering;
+using BookManager.Validators;
+using Microsoft.Extensions.Configuration;
+
+namespace BookManager.Execution
+{
+    public class Facade
+    {
+        public static void Execute(string? path)
+        {
+            ValidatorInputData.ValidateFilePath(path);
+
+            string jsonName = "appSettings.json";
+            string sectionName = "Filters";
+            string pathCSV = OutputDispatcher.GetOutputFileName();
+
+            IConfigurator configManager = new ConfiguratorJson(jsonName);
+            IConfiguration config = configManager.Config;
+
+            using (ApplicationContext context = new(config))
+            {
+                IDatabaseManager databaseManager = new DatabaseManager(context);
+
+                IFileParser parser = new FileParserCSV(databaseManager);
+                parser.Parse(path!);
+
+                Filter filter = configManager.GetFilter(sectionName);
+                Searcher searcher = new(filter, context);
+
+                OutputDispatcher outputer = new(searcher.FillteredBook, (parser as FileParserCSV)?.FileColumnOrder);
+                outputer.OutputToConsole();
+                outputer.OutputToCSV(pathCSV);
+            }
+        }
+    }
+}
