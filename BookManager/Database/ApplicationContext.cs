f9f997dbc235137b458e09c7e49aa690d06ee30f
HoliakaDmytro <holidim39@gmail.com>
2024-01-28 13:16:23 +0200

    ApplicationContext: Remove unnecessary recreating database

--- BookManager/Database/ApplicationContext.cs
+++ BookManager/Database/ApplicationContext.cs
@@ -23,10 +23,6 @@ namespace BookManager.Database
             }
 
             Config = config;
-
-            // ToDo: Remove
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
