4de1f76176403f58197e4c936aa4ba7e45f601f4
HoliakaDmytro <holidim39@gmail.com>
2024-01-29 15:01:35 +0200

    Author: Remove copy constructor

--- BookManager/Entities/Author.cs
+++ BookManager/Entities/Author.cs
@@ -2,27 +2,9 @@
 {
     public class Author
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string? Name { get; set; }   // Required
 
         public ICollection<Book>? Books { get; set; }
-
-        public Author()
-        {
-
-        }
-
-        public Author(Author? origin)
-        {
-            if (origin is null)
-            {
-                throw new ArgumentNullException(nameof(origin), "Origin instance of author cannot be null");
-            }
-
-            Id = origin.Id;
-            Name = origin.Name;
-
-            Books = origin.Books is null ? null : new List<Book>(origin.Books);
-        }
     }
 }
