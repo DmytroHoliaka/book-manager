5bbc7e7edf1cdc1bc166af4d0331e1d4179639e0
HoliakaDmytro <holidim39@gmail.com>
2024-01-30 18:14:44 +0200

    ValidatorInputDataTests: Add tests for ValidatorInputData

---
+++ BookManager.Tests.UnitTesting.xUnit/ValidatorInputDataTests.cs
@@ -0,0 +1,27 @@
+﻿using BookManager.Validators;
+
+namespace BookManager.Tests.UnitTesting.xUnit
+{
+    public class ValidatorInputDataTests
+    {
+        [Fact]
+        public void ValidateFilePath_Null_ThrowsArgumentNullException()
+        {
+            // Arrange
+            string? path = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => ValidatorInputData.ValidateFilePath(path));
+        }
+
+        [Fact]
+        public void ValidateFilePath_NotExistingFile_ThrowsFileNotFoundException()
+        {
+            // Arrange
+            string? path = @".\Unknown path";
+
+            // Act & Assert
+            Assert.Throws<FileNotFoundException>(() => ValidatorInputData.ValidateFilePath(path));
+        }
+    }
+}
