5c9d19f93065e30189f8798ed3acb526bc54b3d1
HoliakaDmytro <holidim39@gmail.com>
2024-01-31 14:35:45 +0200

    Guids: Add constant guids for tests

---
+++ BookManager.Tests.UnitTesting.xUnit/Guids.cs
@@ -0,0 +1,25 @@
+﻿namespace BookManager.Tests.UnitTesting.xUnit
+{
+    public static class Guids
+    {
+        public readonly static Guid Id1 = Guid.NewGuid();
+
+        public readonly static Guid Id2 = Guid.NewGuid();
+
+        public readonly static Guid Id3 = Guid.NewGuid();
+
+        public readonly static Guid Id4 = Guid.NewGuid();
+
+        public readonly static Guid Id5 = Guid.NewGuid();
+
+        public readonly static Guid Id6 = Guid.NewGuid();
+
+        public readonly static Guid Id7 = Guid.NewGuid();
+
+        public readonly static Guid Id8 = Guid.NewGuid();
+
+        public readonly static Guid Id9 = Guid.NewGuid();
+
+        public readonly static Guid Id10 = Guid.NewGuid();
+    }
+}
